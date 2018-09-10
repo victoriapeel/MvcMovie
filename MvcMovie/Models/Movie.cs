724e7eac61f4845183321224473901836a92ebd3
lil Rae <victoriapeel23@outlook.com>
2018-09-09 20:25:38 -0400

    Reformatted column name, formatted date and parsed price

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,9 +11,13 @@ namespace MvcMovie.Models
     {
         public int ID { get; set; }
         public string Title  { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "ReleaseDate")]
+        [DataType(DataType.Date)]
+        public DateTime  ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
